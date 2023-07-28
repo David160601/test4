97f143f486a03cd364d87309e3ea1e117e59baa1
david <phengdavid979@gmail.com>
2023-07-28 19:34:49 +0700

    test

--- src/main/java/com/example/demo/product/dto/CreateProductDto.java
+++ src/main/java/com/example/demo/product/dto/CreateProductDto.java
@@ -4,7 +4,6 @@ import com.example.demo.validation.IdValidation;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.util.List;
